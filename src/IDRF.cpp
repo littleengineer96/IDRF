0b6a588a7040dc27366c3ecce3a30dcd3117b13a
Thiago <thiago@vulcanum.com.br>
2022-05-28 22:22:53 -0300

    #1 organizando codigo
    #1 organizando código:  organizando código em arquivos.

--- src/IDRF.cpp
+++ src/IDRF.cpp
@@ -10,28 +10,8 @@
    GPIO2    2
    RXD      3
 */
-#include <Arduino.h>
-#include <TimeLib.h>
-#include <BlynkSimpleEsp32.h>
-// #include <BlynkSimpleEsp8266.h>
 
-#include <WidgetRTC.h>
-
-//#include <ModbusIP_ESP8266.h>
-#include <DNSServer.h>
-
-// #include <ESP8266WebServer.h>
-
-#include <connection.h>
-#include <globaldef.h>
-// #include <blynkLED.h>
-#include <memoriaFlash.h>
-#include <setupOTA.h>
-#include <setupBlynk.h>
-#include <saveStatus.h>
-#include <isr.h>
-#include <setting.h>
-#include <prototypes.h>
+#include "prototypes.h"
 
 bool bOffLineMode = false;
 bool f_SerialOut_ON = true;
@@ -66,39 +46,19 @@ bool f_Reconnect_Blynk = false;
 unsigned long TimeCheck = 0;
 unsigned long TimeOutConnect = 0;
 unsigned long TimeOut = 0;
-// void inite(uint8_t chg_pin);
 
-Servo servoPan;
-Servo servoTilt;
+String output2State;
+
+// Servo servoPan;
+// Servo servoTilt;
 
 WidgetTerminal terminal(V4);
-
-FastAccelStepperEngine engine = FastAccelStepperEngine();
-FastAccelStepper *stepper = NULL;
 
 void setup()
 {
   Comunication(BAUD_RATE);
 
   Setting_Pins();
-
-  engine.init();
-  stepper = engine.stepperConnectToPin(OUTPIN_A4988_STEP);
-
-  if (stepper)
-  {
-    stepper->setDirectionPin(OUTPIN_A4988_DIR);
-    stepper->setEnablePin(OUTPIN_A4988_EN);
-    stepper->setAutoEnable(true);
-
-    // If auto enable/disable need delays, just add (one or both):
-    // stepper->setDelayToEnable(50);
-    // stepper->setDelayToDisable(1000);
-
-    stepper->setSpeedInUs(1000); // the parameter is us/step !!!
-    stepper->setAcceleration(100);
-    stepper->move(1000);
-  }
 
   Start_Timer(TIME_INTERRUPT);
 
@@ -167,58 +127,32 @@ void loop()
     Blynk.run();
   }
 
-  if (MyMoves.movCanPan)
-  {
-    MyMoves.movCanPan = false;
-    Serial.println("Movendo Camera (PAN) value: " + String(MyMoves.valueCanPan));
-    servoPan.write(MyMoves.valueCanPan);
-  }
-
-  if (MyMoves.movCanTilt)
-  {
-    MyMoves.movCanTilt = false;
-    Serial.println("Movendo Camera (TILT) value: " + String(MyMoves.valueCanTilt));
-    servoTilt.write(MyMoves.valueCanTilt);
-  }
-
-  if (MyMoves.movRoboRight)
-  {
-    MyMoves.movRoboRight = false;
-    Serial.println("Movendo Robo (RIGHT) value: " + String(MyMoves.valueRoboRight));
-  }
-
-  if (MyMoves.movRoboLeft)
-  {
-    MyMoves.movRoboLeft = false;
-    Serial.println("Movendo Robo (LEFT) value: " + String(MyMoves.valueRoboLeft));
-  }
-
   ArduinoOTA.handle();
 }
 
-BLYNK_WRITE(MOV_CAN_PAN)
-{
-  MyMoves.movCanPan = true;
-  MyMoves.valueCanPan = param.asInt();
-}
+// BLYNK_WRITE(MOV_CAN_PAN)
+// {
+//   MyMoves.movCanPan = true;
+//   MyMoves.valueCanPan = param.asInt();
+// }
 
-BLYNK_WRITE(MOV_CAN_TILT)
-{
-  MyMoves.movCanTilt = true;
-  MyMoves.valueCanTilt = param.asInt();
-}
+// BLYNK_WRITE(MOV_CAN_TILT)
+// {
+//   MyMoves.movCanTilt = true;
+//   MyMoves.valueCanTilt = param.asInt();
+// }
 
-BLYNK_WRITE(MOV_ROBO_RIGHT)
-{
-  MyMoves.movRoboRight = true;
-  MyMoves.valueRoboRight = param.asInt();
-}
+// BLYNK_WRITE(MOV_ROBO_RIGHT)
+// {
+//   MyMoves.movRoboRight = true;
+//   MyMoves.valueRoboRight = param.asInt();
+// }
 
-BLYNK_WRITE(MOV_ROBO_LEFT)
-{
-  MyMoves.movRoboLeft = true;
-  MyMoves.valueRoboLeft = param.asInt();
-}
+// BLYNK_WRITE(MOV_ROBO_LEFT)
+// {
+//   MyMoves.movRoboLeft = true;
+//   MyMoves.valueRoboLeft = param.asInt();
+// }
 
 BLYNK_WRITE(RESET_WIFI_V255)
 {
